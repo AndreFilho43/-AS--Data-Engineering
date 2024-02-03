59b98743875eda6c64e263513f2375ef0309db7a
MrLord434 <andre.filho43@outlook.com>
2024-02-02 22:13:33 -0300

    Estilização

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-function soma() {
+function obrigado() {
     var a = document.getElementById("nome").value;
     var b = document.getElementById("sobrenome").value;
     return alert(`Obrigado pelo contato, ${a} ${b}!`)
 }
 
-document.getElementById("clickMe").onclick = soma;
+document.getElementById("envio-formulario").onclick = obrigado;
 
 
